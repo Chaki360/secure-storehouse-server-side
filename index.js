ee8245a33bf109d207f30cf530d86716992d8970
Badhon chaki <fboy4069@gmail.com>
2022-05-08 11:06:33 +0600

    data inserted on db

--- index.js
+++ index.js
@@ -25,7 +25,20 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const inventories = await inventoryCollection.findOne(query);
             res.send(inventories);
-        })
+        });
+        app.post('/inventory', async (req, res) => {
+            const newInventory = req.body;
+            const result = await inventoryCollection.insertOne(newInventory);
+            res.send(result);
+
+        });
+
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await inventoryCollection.deleteOne(query);
+            res.send(result);
+        });
 
     }
     finally {
@@ -36,9 +49,7 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Running Warehouse Server');
 });
-app.get('/', (req, res) => {
-    res.send('Running Warehouse Server');
-});
+
 app.listen(port, () => {
     console.log('Listening something', port)
 })
